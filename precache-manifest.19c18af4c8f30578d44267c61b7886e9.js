a7d7c0b56851970015442d3d6bff38094b5e6944
mukul banga <you@example.com>
2020-06-30 11:06:59 +0530

    Updates

---
+++ precache-manifest.19c18af4c8f30578d44267c61b7886e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb9a30a813d7c108ffeebbfdef712e6",
+    "url": "/ghssa/index.html"
+  },
+  {
+    "revision": "734364c0da001a1e2222",
+    "url": "/ghssa/static/css/main.9789373e.chunk.css"
+  },
+  {
+    "revision": "c1302d5b6e113f6aa4a2",
+    "url": "/ghssa/static/js/2.54865750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghssa/static/js/2.54865750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734364c0da001a1e2222",
+    "url": "/ghssa/static/js/main.6d8ea8c4.chunk.js"
+  },
+  {
+    "revision": "74e2c56f6f6d3ea89d2f",
+    "url": "/ghssa/static/js/runtime-main.692c8e03.js"
+  },
+  {
+    "revision": "2c258c5907ad3fe3a662be6c70da7723",
+    "url": "/ghssa/static/media/pic.2c258c59.jpg"
+  }
+]);
